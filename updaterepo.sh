3f413c0a8b36fd4e1d420b30ae3c8fc06eb5b586
Sakibur Rahman <sakibmas@gmail.com>
2021-10-03 00:27:52 +0600

    Update updaterepo.sh

--- updaterepo.sh
+++ updaterepo.sh
@@ -8,10 +8,10 @@ do
  date
  echo -e "reposync --repoid=$rid --download_path=/var/www/html --downloadcomps --download-metadata --gpgcheck"
  echo -e "$ssep"
- reposync --repoid=$rid --download_path=/var/www/html --downloadcomps --download-metadata --gpgcheck
+ reposync --repoid=$rid --download_path=/var/www/html --delete --downloadcomps --download-metadata --gpgcheck
  echo -e "$dsep"
 done
 createrepo --update /var/www/html
 createrepo -g /var/www/html/repodata/*comps*.xml /var/www/html
-yum -y update
+yum -y --releasever=7.7 update
 /usr/sbin/reboot
