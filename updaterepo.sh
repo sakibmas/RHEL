4f5e57698aa6a68c0a6e8ce847b358bb65069c69
Sakibur Rahman <sakibmas@gmail.com>
2021-10-03 08:44:57 +0600

    Update updaterepo.sh

--- updaterepo.sh
+++ updaterepo.sh
@@ -6,12 +6,13 @@ for rid in $(subscription-manager repos --list | grep -B3 "Enabled:   1" | grep 
 do
  # logp=/var/log/updaterepo-$rid-$(date +%Y%m%d%H%M).log
  date
- echo -e "reposync --repoid=$rid --download_path=/var/www/html --downloadcomps --download-metadata --gpgcheck"
+ echo -e "reposync --repoid=$rid --download_path=/var/www/html"
  echo -e "$ssep"
  reposync --repoid=$rid --download_path=/var/www/html --delete --downloadcomps --download-metadata --gpgcheck
  echo -e "$dsep"
 done
-createrepo --update /var/www/html
-createrepo -g /var/www/html/repodata/*comps*.xml /var/www/html
+echo -e "Creating xml-based rpm metadata"
+createrepo --verbose --update /var/www/html
+createrepo --verbose --groupfile /var/www/html/repodata/*comps*.xml /var/www/html
 yum -y --releasever=7.7 update
 /usr/sbin/reboot
