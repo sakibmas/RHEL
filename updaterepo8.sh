362b0e327bd11d0f0b5ba10fcee52df67412bef2
Sakibur Rahman <sakibmas@gmail.com>
2023-05-03 21:57:49 +0600

    Update updaterepo8.sh

--- updaterepo8.sh
+++ updaterepo8.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 . ~/.bashrc
-ssep="-----------------------------------------------------------------------------------"
 dsep="==================================================================================="
 for rid in $(subscription-manager repos --list | grep -B3 "Enabled:   1" | grep "Repo ID:" | cut -d" " -f5)
 do
  date
  echo -e "reposync --repoid=$rid --download-path=/var/www/html"
- echo -e "$ssep"
- reposync --repoid=$rid --download-path=/var/www/html --downloadcomps --download-metadata
- echo -e "$ssep\nCreating xml-based group file\n$ssep"
+ echo -e "$dsep"
+ reposync --repoid=$rid --download-path=/var/www/html --downloadcomps --download-metadata --delete --gpgcheck
+ echo -e "$dsep\nCreating xml-based group file\n$dsep"
  createrepo --verbose -g /var/www/html/$rid/repodata/repomd.xml /var/www/html/$rid/repodata
  echo -e "$dsep"
 done
-echo -e "$ssep\nCreating xml-based repo file\n$ssep"
+echo -e "$dsep\nCreating xml-based repo file\n$dsep"
 createrepo --update --verbose /var/www/html
 yum -y --releasever=8.4 update
 /usr/sbin/reboot
